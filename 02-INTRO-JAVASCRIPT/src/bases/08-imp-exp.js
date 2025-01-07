8cb5a8a44a97b6b2fb82f5dfdeff8d5a40fb3eb4
fcodev <fcodev@gmail.com>
2025-01-07 12:06:07 -0600

    promesas added

--- 02-INTRO-JAVASCRIPT/src/bases/08-imp-exp.js
+++ 02-INTRO-JAVASCRIPT/src/bases/08-imp-exp.js
@@ -3,23 +3,23 @@
 // 2.
 // import cualquierNombre from "./data/heroes";
 // 3.  import por defecto más otras importaciones
-import heroes, { owners } from "../data/heroes";
+import heroes from "../data/heroes";
 // 4.
 // import { heroes, owners } from "./data/heroes";
 // 5. 
 
-const getHeroById = (id) => {
+export const getHeroById = (id) => {
     return heroes.find((e) => e.id === id);
 }
 
-console.log(getHeroById(2));
+// console.log(getHeroById(2));
 
-const getHeroByOwner = (owner) => {
+export const getHeroByOwner = (owner) => {
     return heroes.filter((e) => e.owner === owner);
 }
 
-console.log(getHeroByOwner('DC'));
-console.log(owners);
+// console.log(getHeroByOwner('DC'));
+// console.log(owners);
 
 
 
---
+++ 02-INTRO-JAVASCRIPT/src/bases/09-promesas.js
@@ -0,0 +1,43 @@
+import { getHeroById } from "./bases/08-imp-exp";
+
+
+// * PROMESAS
+
+// const promesa = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         const hero = getHeroById(2);
+
+//         resolve(hero);
+//         // reject('No se pudo encontrar el heroe')
+
+//     }, 1000);
+
+// });
+
+// promesa.then((data) => {
+//     console.log('Heroe', data);
+// }).catch((err) => {
+//     console.warn(err);
+// });
+
+const getHeroByIdAsync = (id) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            const hero = getHeroById(id);
+            if (!hero) {
+                reject(`No se pudo encontrar el heroe: ${id}`)
+
+            }
+            resolve(hero);
+
+        }, 1000);
+
+    });
+}
+// ? Si la funcion tiene un solo parametro y solo 1 se recibe
+getHeroByIdAsync(1)
+    .then(console.log)
+    .catch(console.warn);
+
+
+
